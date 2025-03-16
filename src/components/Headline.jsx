0943c4461668572bc806a74f42a97fffd7c94249
A-bahaa <ghonim.ce@gmail.com>
2025-03-16 11:26:58 +0200

    feat/ create headline component

---
+++ src/components/Headline.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../styles/headline.css';
+const Headline = () => {
+  return (
+    <div className="headline-wrap">
+      <h2 className="headline">TRUMP IS INDICTED OVER CLASSIFIED FILES</h2>
+    </div>
+  );
+};
+
+export default Headline;

ab779f74dd9b83603bc7bf517266055335143edd
A-bahaa <ghonim.ce@gmail.com>
2025-03-16 06:50:17 +0200

    refactor the usePageDimensions hook
    change the page ratio to 6/11 to stick to Sho Shibuya's work
    chang the pageMargin to 90% for better look
    UseLayoutEffect instead of useEffect to get the dimensions before the browser paint the page
    set global css variables for page-width and page-height insted of returning them as a state

--- src/hooks/usePageDimensions.js
+++ src/hooks/usePageDimensions.js
@@ -1,10 +1,58 @@
-import { useEffect, useState } from 'react';
+import { useLayoutEffect } from 'react';
+const usePageDimensions = () => {
+  const pageRatio = 6 / 11;
+  const pageMargin = 0.9;
+
+  useLayoutEffect(() => {
+    const windowSizeHandler = () => {
+      const current_height = document.documentElement.clientHeight * pageMargin;
+      const current_width = document.documentElement.clientWidth * pageMargin;
+      const page_dimensions = [];
+      // landscape orientation
+      // take the whole height and set the width accordingally
+      if (current_width >= current_height * pageRatio) {
+        [page_dimensions[0], page_dimensions[1]] = [
+          current_height * pageRatio,
+          current_height,
+        ];
+      } else {
+        // portrait orientation
+        // take the whole width and set the height accordingally
+        [page_dimensions[0], page_dimensions[1]] = [
+          current_width,
+          current_width / pageRatio,
+        ];
+      }
+      document.documentElement.style.setProperty(
+        '--page-width',
+        `${page_dimensions[0]}px`
+      );
+      document.documentElement.style.setProperty(
+        '--page-height',
+        `${page_dimensions[1]}px`
+      );
+    };
+    // set the dimention on the first mount
+    windowSizeHandler();
+    window.addEventListener('resize', windowSizeHandler);
+    // cleanup
+    return () => {
+      window.removeEventListener('resize', windowSizeHandler);
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+};
+
+export default usePageDimensions;
+
+/*
+import { useLayoutEffect, useState } from 'react';
 const usePageDimensions = () => {
   const [dimensions, setDimensions] = useState([]);
-  const pageRatio = 8 / 11;
-  const pageMargin = 1;
+  const pageRatio = 6 / 11;
+  const pageMargin = 0.9;
 
-  useEffect(() => {
+  useLayoutEffect(() => {
     const windowSizeHandler = () => {
       const current_height = document.documentElement.clientHeight * pageMargin;
       const current_width = document.documentElement.clientWidth * pageMargin;
@@ -17,6 +65,10 @@ const usePageDimensions = () => {
         // take the whole width and set the height accordingally
         setDimensions([current_width, current_width / pageRatio]);
       }
+      document.documentElement.style.setProperty(
+        '--my-page-width',
+        `${current_height * pageRatio * 0.09}px`
+      );
     };
     // set the dimention on the first mount
     windowSizeHandler();
@@ -27,8 +79,12 @@ const usePageDimensions = () => {
     };
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
-
+  console.log(dimensions);
   return dimensions;
 };
 
 export default usePageDimensions;
+
+
+
+*/

f6ef733bd64ff01c7096c2521216eb7c91fa3ba1
A-bahaa <ghonim.ce@gmail.com>
2025-03-16 11:27:46 +0200

    feat/ create folio component

---
+++ src/components/Folio.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../styles/folio.css';
+
+const Folio = () => {
+  return (
+    <div className="folio-wrap ">
+      <span>VOL. I .... No. 1</span>
+      <span style={{ justifySelf: 'center' }}> THURSDAY, MARCH 13, 2025</span>
+      <span>$0.00</span>
+    </div>
+  );
+};
+
+export default Folio;

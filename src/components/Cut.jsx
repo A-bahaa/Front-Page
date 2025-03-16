ab039ac4edce4d599c6ec18c6e91b10edb7f2d52
A-bahaa <ghonim.ce@gmail.com>
2025-03-16 11:28:48 +0200

    feat/ create cut component

---
+++ src/components/Cut.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Cut = () => {
+  return (
+    <div
+      style={{
+        width: '100%',
+        /*border: '1px solid red',*/
+        flexGrow: 1,
+        marginTop: '2%',
+        overflow: 'hidden',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <img src="test9.jpg" style={{ height: '100%' }} />
+    </div>
+  );
+};
+
+export default Cut;

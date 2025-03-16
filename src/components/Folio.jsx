26655bedd287f18fcab39d2d9885ab798bba0b18
A-bahaa <ghonim.ce@gmail.com>
2025-03-16 19:28:23 +0200

    refactor folio component
    remove inline styling and use css vars

--- src/components/Folio.jsx
+++ src/components/Folio.jsx
@@ -5,7 +5,7 @@ const Folio = () => {
   return (
     <div className="folio-wrap ">
       <span>VOL. I .... No. 1</span>
-      <span style={{ justifySelf: 'center' }}> THURSDAY, MARCH 13, 2025</span>
+      <span> THURSDAY, DECEMBER 13, 2025</span>
       <span>$0.00</span>
     </div>
   );

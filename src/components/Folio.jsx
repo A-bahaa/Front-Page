9b7a7b1facd5aaedca5601a929b0f7601a62cf75
A-bahaa <ghonim.ce@gmail.com>
2025-03-20 07:51:25 +0200

    Parse the formatted date in the folio from a dynamic date object

--- src/components/Folio.jsx
+++ src/components/Folio.jsx
@@ -2,10 +2,19 @@ import React from 'react';
 import '../styles/folio.css';
 
 const Folio = () => {
+  const options = {
+    weekday: 'long',
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+  };
+  const formattedDate = new Date()
+    .toLocaleDateString('en-US', options)
+    .toUpperCase();
   return (
     <div className="folio-wrap ">
       <span>VOL. I .... No. 1</span>
-      <span> THURSDAY, DECEMBER 13, 2025</span>
+      <span> {formattedDate}</span>
       <span>$0.00</span>
     </div>
   );

29b16ffab50b53f8ef0e68dba04c3dbf43bcb6b3
A-bahaa <ghonim.ce@gmail.com>
2025-03-20 07:12:38 +0200

    Structure the editorial page

--- src/pages/Editorial.jsx
+++ src/pages/Editorial.jsx
@@ -1,7 +1,32 @@
 import React from 'react';
+import Page from '../components/Page';
+import Panel from '../components/Panel';
+import '../styles/editorial.css';
 
 const Editorial = () => {
-  return <div>Editorial</div>;
+  return (
+    <div
+      className="editorial-wrap"
+      style={{
+        border: '1px solid red',
+      }}
+    >
+      <div
+        className="editorial-page-wrap"
+        style={{
+          border: '1px solid black',
+        }}
+      >
+        <Page />
+      </div>
+      <div
+        style={{ border: '1px solid blue' }}
+        className="editorial-panel-wrap"
+      >
+        <Panel />
+      </div>
+    </div>
+  );
 };
 
 export default Editorial;

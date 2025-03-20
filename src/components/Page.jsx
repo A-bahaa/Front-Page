4e100e282735b6ab33466a3518e32a4597a116e8
A-bahaa <ghonim.ce@gmail.com>
2025-03-20 06:30:20 +0200

    feat/ the page component changes bg image on doubleClick

--- src/components/Page.jsx
+++ src/components/Page.jsx
@@ -1,15 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../styles/page.css';
-import usePageDimensions from '../hooks/usePageDimensions';
 import Masthead from './Masthead';
 import Folio from './Folio';
 import Headline from './Headline';
 import Cut from './Cut';
 
 const Page = () => {
-  usePageDimensions();
+  const [currentBgIndex, setCurrentBgIndex] = useState(0);
+
+  const backgroundImages = [
+    '../../public/background.jpg',
+    '../../public/background2.jpg',
+    '../../public/background3.jpg',
+    '../../public/background4.jpg',
+    '../../public/background5.jpg',
+    '../../public/background6.jpg',
+  ];
+
+  const handleChangeBg = () => {
+    const newImageIndex = (currentBgIndex + 1) % backgroundImages.length;
+    const newImage = new Image();
+    newImage.src = backgroundImages[newImageIndex];
+    // wait untill the new image is uploaded then change the state
+    newImage.onload = () => {
+      setCurrentBgIndex(newImageIndex);
+    };
+  };
+
   return (
-    <div className="page">
+    <div
+      className="page"
+      style={{ backgroundImage: `url(${backgroundImages[currentBgIndex]})` }}
+      onDoubleClick={handleChangeBg}
+    >
       <div className="page-wrap">
         <Masthead />
         <Folio></Folio>

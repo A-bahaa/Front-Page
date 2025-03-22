ef13d511e74e1fb37ee90c06ea4f187e9eecc502
A-bahaa <ghonim.ce@gmail.com>
2025-03-23 01:37:22 +0200

    feat/ create the panel headline component

---
+++ src/components/PanelHeadline.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import '../styles/panelHeadline.css';
+
+const PanelHeadline = ({
+  handleChangeHeadline,
+  headlineFontSize,
+  headlineFontStyle,
+}) => {
+  return (
+    <div className="panel-headline-wrap">
+      <label htmlFor="headline" className="visually-hidden">
+        Headline
+      </label>
+      <input
+        className="panel-headline-input"
+        type="text"
+        id="headline"
+        placeholder="Write the slickest headline you can think of!"
+        onChange={handleChangeHeadline}
+        aria-describedby="type the headline of your story"
+        style={{
+          fontSize: `calc(var(--page-width) * ${headlineFontSize})`,
+          fontStyle: headlineFontStyle,
+        }}
+        maxLength={50}
+      />
+    </div>
+  );
+};
+
+export default PanelHeadline;

b5cc80417a11cc7f00603c36e4103ebe96c857bf
A-bahaa <ghonim.ce@gmail.com>
2025-03-24 02:24:54 +0200

    create a Byline component to display the name of the user

---
+++ src/components/Byline.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/byline.css';
+
+const Byline = () => {
+  return (
+    <div className="byline" aria-describedby="name of the journal's author">
+      By Ahmed Ghonim
+    </div>
+  );
+};
+
+export default Byline;

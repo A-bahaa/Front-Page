133116cd1e06b56440b13bbab9b5eeaa97b928da
A-bahaa <ghonim.ce@gmail.com>
2025-03-28 08:34:48 +0200

    create a landing page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,20 +1,37 @@
 import React from 'react';
-import Page from '../components/Page';
+import '../styles/home.css';
+import Logo from '../components/Logo';
+import home1 from '../assets/images/home1.png';
+import home2 from '../assets/images/home2.png';
+import home3 from '../assets/images/home3.png';
+import home4 from '../assets/images/home4.png';
+import { Button } from 'antd';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        border: '1px solid black',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontFamily: 'chomsky',
-        fontSize: '300px',
-      }}
-    >
-      The Front Page
+    <div className="home-wrap">
+      <div className="logo-cnt">
+        <Logo />
+      </div>
+      <div className="vertical-marquee-container">
+        <div className="overlay">
+          <Button
+            className="action-btn"
+            color="default"
+            variant="solid"
+            aria-describedby="go to the editorial page"
+          >
+            <Link to="/editorial">Start Journaling</Link>
+          </Button>
+        </div>
+        <div className="vertical-marquee-content">
+          <img src={home1} alt="landing-image-1" />
+          <img src={home2} alt="landing-image-2" />
+          <img src={home3} alt="landing-image-3" />
+          <img src={home4} alt="landing-image-4" />
+        </div>
+      </div>
     </div>
   );
 };
